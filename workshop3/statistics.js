5531d0c3f43e50032d5e0efd26c9de02c57768a5
MoonHack2077 <arenasrinconj@gmail.com>
2022-01-14 13:48:50 -0500

    Added function to find the middle

--- workshop3/statistics.js
+++ workshop3/statistics.js
@@ -1,2 +1,20 @@
 const average = arr => arr.reduce((acc,el)=> acc+el) / arr.length;
+
+
+const middle = arr =>{
+    arr = arr.sort((a,b)=> a-b);
+    console.log(arr);
+    if(arr.length % 2 === 0){
+        const mit1= arr.length / 2;
+        const mit2= mit1-1;
+        const av = (arr[mit1] + arr[mit2]) / 2
+        return av;
+    }else{
+        const mit = Math.floor(arr.length/2);
+        const po = arr[mit];
+        return po;
+    }
+}
+
 console.log(average([100,200,300,400,500]));
+console.log(middle([200,50,30,100,50,10,7]));
