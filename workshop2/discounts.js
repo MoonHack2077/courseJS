70ff4170298d8b1b32c24c8c4aaf12a032e69596
MoonHack2077 <arenasrinconj@gmail.com>
2022-01-13 23:07:51 -0500

    Switch from direct discounts to coupons

--- workshop2/discounts.js
+++ workshop2/discounts.js
@@ -1,13 +1,24 @@
 const discount =( price, percentage )=> ( price * ( 100 - percentage) ) / 100
 
 const totalPrice = document.getElementById('price');
-const discountPercentage = document.getElementById('discount');
+const coupon = document.getElementById('coupons');
 const btn = document.getElementById('btn');
 const result = document.getElementById('result');
 
+const coupons=[
+    { value:'0', discountPer: 15},
+    { value:'1', discountPer: 20},
+    { value:'2', discountPer: 50}
+]
+
 btn.addEventListener('click', ()=>{
     const totalPriceValue = parseInt(totalPrice.value);
-    const discountPercentageValue = parseInt(discountPercentage.value);
+    const couponValue = coupon.value;
+    const find = coupons.find( c => c.value === couponValue);
+    console.log(find);
+    if(find){
+        result.innerHTML=`<b>The total to pay is:</b> $${discount(totalPriceValue, find.discountPer)} <br>`;
+    }
 
-    result.innerHTML=`<b>The total to pay is:</b> $${discount(totalPriceValue, discountPercentageValue)} <br>`;
+    
 })
