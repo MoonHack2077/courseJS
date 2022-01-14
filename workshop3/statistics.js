6b07c0edb5950205fe8f6ef4a7791c711bed5540
MoonHack2077 <arenasrinconj@gmail.com>
2022-01-14 15:17:35 -0500

    Added function to find the fashion

--- workshop3/statistics.js
+++ workshop3/statistics.js
@@ -3,7 +3,6 @@ const average = arr => arr.reduce((acc,el)=> acc+el) / arr.length;
 
 const middle = arr =>{
     arr = arr.sort((a,b)=> a-b);
-    console.log(arr);
     if(arr.length % 2 === 0){
         const mit1= arr.length / 2;
         const mit2= mit1-1;
@@ -16,5 +15,21 @@ const middle = arr =>{
     }
 }
 
+const fashion= arr =>{
+    const list = {};
+    
+    arr.map( x => {
+        if(list[x]){
+            list[x]+=1;
+        }else{
+            list[x]=1
+        }
+    })
+
+    const fash= Object.entries(list).sort((a,b)=> a[1]-b[1]);
+    return fash.pop()[0];
+}
+
 console.log(average([100,200,300,400,500]));
 console.log(middle([200,50,30,100,50,10,7]));
+console.log(fashion([1,2,3,4,5]));
