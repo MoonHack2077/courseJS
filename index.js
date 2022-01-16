41af2fd18a37c8767a3303724749d3ed1b553ac2
MoonHack2077 <arenasrinconj@gmail.com>
2022-01-16 18:20:52 -0500

    Very nice :)

---
+++ index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    // const first_work = document.getElementById('first_work');
+    // const second_work = document.getElementById('second_work');
+    // const third_work = document.getElementById('third_work');
+
+    // first_work.onmouseup=()=>{
+    //     window.open('workshop1/figures.html');
+    // }
+    // second_work.onclick=()=>{
+    //     window.open('workshop2/discounts.html');
+    // }
+    // third_work.onclick=()=>{
+    //     window.open('workshop3/statistics.html');
+    // }
+})
