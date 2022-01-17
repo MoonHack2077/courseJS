de27450abefe7909812fb7d8cdf3f62ccd4115a8
MoonHack2077 <arenasrinconj@gmail.com>
2022-01-17 14:45:47 -0500

    Age ready (or so i think)

--- index.js
+++ index.js
@@ -9,29 +9,14 @@ document.addEventListener('DOMContentLoaded',()=>{
     
     const date  = new Date();
     const years  = date.getFullYear()-2004;
-    const hour  = date.getHours();
-    const minute  = date.getMinutes();
-    const seconds  = date.getSeconds();
     const month  = date.getMonth();
     const day = date.getDate();
 
-    if( month<4){
+    if( month<4 ){
         age.innerHTML=`I´m ${years-1} years old`
-    }else if( month>=4){
-        if(day>=22){
-            age.innerHTML=`I´m ${years} years old`
-        }else{
+    }else if( month===4 && day<22 ){
             age.innerHTML=`I´m ${years-1} years old`
-        }
+    }else{
+        age.innerHTML=`I´m ${years} years old`
     }
-
-    
-    
-
-    // console.log(date);
-    // console.log(month);
-    // console.log(hour)
-    // console.log(minute);
-    // console.log(seconds);
-    // console.log(years);
 })
