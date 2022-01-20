0fb2f5872235c1ddfc07a2dac4e10e7c2d9ac686
MoonHack2077 <arenasrinconj@gmail.com>
2022-01-20 18:33:42 -0500

    better organize

--- workshop2/discounts.js
+++ workshop2/discounts.js
@@ -1,24 +1,25 @@
-const discount =( price, percentage )=> ( price * ( 100 - percentage) ) / 100
+const discount = ( price, percentage ) => Math.round(( price * ( 100 - percentage) ) / 100);
 
 const totalPrice = document.getElementById('price');
-const coupon = document.getElementById('coupons');
+const discountPercentage = document.getElementById('discount');
 const btn = document.getElementById('btn');
 const result = document.getElementById('result');
 
-const coupons=[
-    { value:'0', discountPer: 15},
-    { value:'1', discountPer: 20},
-    { value:'2', discountPer: 50}
-]
-
 btn.addEventListener('click', ()=>{
     const totalPriceValue = parseInt(totalPrice.value);
-    const couponValue = coupon.value;
-    const find = coupons.find( c => c.value === couponValue);
-    console.log(find);
-    if(find){
-        result.innerHTML=`<b>The total to pay is:</b> $${discount(totalPriceValue, find.discountPer)} <br>`;
-    }
+    const discountPercentageValue = parseFloat(discountPercentage.value);
 
-    
-})
+    if( totalPriceValue<0 && discountPercentageValue<0 ){
+        result.innerHTML='<b>What?, Negative?</b>'
+    }else if( totalPriceValue<0  ){
+        result.innerHTML='<b>Price negative?</b>'
+    }else if( discountPercentageValue<0 ){
+        result.innerHTML='<b>Discount negative?</b>'
+    }else if( !(isNaN(totalPriceValue) || isNaN(discountPercentageValue)) ){
+        if( discountPercentageValue > 100 ){
+            result.innerHTML='<b>What?, discont greater than 100%?</b>'
+        }else{
+            result.innerHTML=`<b>The total price to pay is: $${discount(totalPriceValue, discountPercentageValue)}</b>`
+        }
+    }
+})  
