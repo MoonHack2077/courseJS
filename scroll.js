b0887ec1fb1cbc6da8fa040e11997990ea38f2fc
MoonHack2077 <arenasrinconj@gmail.com>
2022-01-21 22:09:48 -0500

    READYgit statusgit status

--- scroll.js
+++ scroll.js
@@ -8,27 +8,48 @@ document.addEventListener('DOMContentLoaded',()=>{
         const currentScroll= document.documentElement.scrollTop || document.body.scrollTop;
         if( currentScroll > 0 ){
             // window.requestAnimationFrame(scrollUp);
-            window.scrollTo( 0, 100 );
+            window.scrollTo( 0, 0 );
         }
     }
     arrow.addEventListener('click', scrollUp);
     
     window.onscroll=()=>{
         const currentScroll= document.documentElement.scrollTop || document.body.scrollTop;
-        if( currentScroll > 900 ){
-            arrow.style.transform= 'scale(1)'
-        }else if( currentScroll<900 ){
-            arrow.style.transform= 'scale(0)'
+        if(window.innerWidth <= 977 ){
+            if( currentScroll > 900 ){
+                arrow.style.transform= 'scale(1)'
+            }else if( currentScroll<900 ){
+                arrow.style.transform= 'scale(0)'
+            }
+        }else{
+            if( currentScroll > 680 ){
+                arrow.style.transform= 'scale(1)'
+            }else if( currentScroll < 700 ){
+                arrow.style.transform= 'scale(0)'
+            }
         }
+
     }
 
     geometryFigures.onclick=()=>{
-        window.scrollTo( 0,1110 );
+        if(window.innerWidth <= 977 ){
+            window.scrollTo( 0,1110 );
+        }else{
+            window.scrollTo( 0,700 );
+        }
     }
     discount.onclick=()=>{
-        window.scrollTo( 0,2000 );
+        if(window.innerWidth <= 977 ){
+            window.scrollTo( 0,2000 );
+        }else{
+            window.scrollTo( 0,1595 );
+        }
     }
     stats.onclick=()=>{
-        window.scrollTo( 0,2600 );
+        if(window.innerWidth <= 977 ){
+            window.scrollTo( 0,2600 );
+        }else{
+            window.scrollTo( 0,2600 );
+        }
     }
 })
